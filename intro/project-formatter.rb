c48bf33ea0a0f88c7ce825cf3b61e15f65e177b4
BENJAMIN CEA <benjaminceavidal@gmail.com>
2020-12-20 11:28:59 -0600

    Commented code

--- intro/project-formatter.rb
+++ intro/project-formatter.rb
@@ -1,5 +1,11 @@
+# print logs into console
+# puts prints a new line while print doesn't
 print "What's your name?"
+# gets is how you ask user for input and chomp takes the new line that
+# get comes with
 first_name = gets.chomp
+# capitalize is a method to capitalize a string
+# ! the bang is used to modify the value of the variable
 first_name.capitalize!
 
 print "What's your last name?"
@@ -12,6 +18,8 @@ city.capitalize!
 
 print "What state do live in?"
 state = gets.chomp
+# upcase method is used to make all characters upper case
 state.upcase!
 
+# #{} between quotes is to put a variable
 print "#{first_name} #{last_name}, you live in #{city}, #{state}"
