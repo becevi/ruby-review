1cbfca00acc58ea5100d719f3ae96c883cf926c2
BENJAMIN CEA <benjaminceavidal@gmail.com>
2020-12-20 14:59:06 -0600

    Commented code

--- control_flow/project-daffy-duck.rb
+++ control_flow/project-daffy-duck.rb
@@ -2,7 +2,17 @@ print "Say something with an 's' you bastard!!"
 user_input = gets.chomp
 user_input.downcase!
 
+# if statement works like this:
+# if is followed by condition
+# code executes if true
+# write else
+# code to execute if false
+# write end to close block
+
+# include? checks if value contains s, it evaluates to true or false 
 if user_input.include? "s"
+# global substitution, it changes the character between the dashes to that in
+# quotes
   user_input.gsub!(/s/, "th")
   puts "You thaid #{user_input}"
 else
